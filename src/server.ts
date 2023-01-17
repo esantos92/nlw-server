c13b359056dffb64de184bcc5bcbd726df85bf20
Everton Santos <everton.santos@redealumni.com>
2023-01-16 21:08:34 -0300

    back end setup finished.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import Fastify from 'fastify';
 import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors';
 
 const app = Fastify();
 const prisma = new PrismaClient();
 const port = 3333;
+
+app.register(cors)
 
 app.get('/', async () => {
   const habits = await prisma.habit.findMany()
