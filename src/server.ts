0a996bf9899a35ca2f78ab3e1c74d1a93fc5d423
Everton Santos <everton.santos@redealumni.com>
2023-01-16 21:01:48 -0300

    Created first route and prisma configured.

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
-console.log('hello world!!!')
+import Fastify from 'fastify';
+import { PrismaClient } from '@prisma/client'
+
+const app = Fastify();
+const prisma = new PrismaClient();
+const port = 3333;
+
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany()
+  return habits;
+})
+
+app.listen({
+  port: port,
+}).then(() => {
+  console.log(`Runnig server on http://localhost:${port}`)
+});
